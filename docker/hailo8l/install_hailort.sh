1f7dc4d5eedc2a41722f55b499858a334f2b74ef
Jan Čermák <sairon@sairon.cz>
2024-11-22 15:08:11 +0100

    Use custom HailoRT repo and version for arm64
    Raspberry Pi kernel only has 4.18.0 version of the driver included. The
    driver version is enforced and 4.19.0 userspace can't use the old
    version. Use custom version from another repo for the time being.

--- docker/hailo8l/install_hailort.sh
+++ docker/hailo8l/install_hailort.sh
@@ -3,17 +3,20 @@
 set -euxo pipefail
 
 hailo_version="4.19.0"
+release_repo="https://github.com/frigate-nvr/hailort"
 
 if [[ "${TARGETARCH}" == "amd64" ]]; then
     arch="x86_64"
 elif [[ "${TARGETARCH}" == "arm64" ]]; then
     arch="aarch64"
+    hailo_version="4.18.0"
+    release_repo="https://github.com/sairon/hailort"
 fi
 
-wget -qO- "https://github.com/frigate-nvr/hailort/releases/download/v${hailo_version}/hailort-${TARGETARCH}.tar.gz" |
+wget -qO- "${release_repo}/releases/download/v${hailo_version}/hailort-${TARGETARCH}.tar.gz" |
     tar -C / -xzf -
 
 mkdir -p /hailo-wheels
 
-wget -P /hailo-wheels/ "https://github.com/frigate-nvr/hailort/releases/download/v${hailo_version}/hailort-${hailo_version}-cp39-cp39-linux_${arch}.whl"
+wget -P /hailo-wheels/ "${release_repo}/releases/download/v${hailo_version}/hailort-${hailo_version}-cp39-cp39-linux_${arch}.whl"
 
