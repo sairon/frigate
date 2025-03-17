5f8de3986a2a0c98ba340841b4f4c42c9b33de26
Jan Čermák <sairon@sairon.cz>
2025-03-17 13:59:22 +0100

    Use Debian 11 build of hailort

--- docker/hailo8l/install_hailort.sh
+++ docker/hailo8l/install_hailort.sh
@@ -10,10 +10,10 @@ elif [[ "${TARGETARCH}" == "arm64" ]]; then
     arch="aarch64"
 fi
 
-wget -qO- "https://github.com/sairon/frigate-hailort/releases/download/v${hailo_version}/hailort-${TARGETARCH}.tar.gz" |
+wget -qO- "https://github.com/sairon/frigate-hailort/releases/download/v${hailo_version}-deb11/hailort-${TARGETARCH}.tar.gz" |
     tar -C / -xzf -
 
 mkdir -p /hailo-wheels
 
-wget -P /hailo-wheels/ "https://github.com/sairon/frigate-hailort/releases/download/v${hailo_version}/hailort-${hailo_version}-cp39-cp39-linux_${arch}.whl"
+wget -P /hailo-wheels/ "https://github.com/sairon/frigate-hailort/releases/download/v${hailo_version}-deb11/hailort-${hailo_version}-cp39-cp39-linux_${arch}.whl"
 
